da68f49271325c3b23c61492ae0cf1cdf967f901
ligradee <57723993+ligradee@users.noreply.github.com>
2020-04-28 22:34:17 +0300

    Update main.py
    Add iterating of cities and pages.

--- main.py
+++ main.py
@@ -153,12 +153,27 @@ def getPageData(html):
 				'linkInst':linkInst,
 				'linkFacebook':linkFacebook,
 				'linkProfi':linkProfi}
+		writeCsv(data)
 
 
 def main():
-	url = "https://yandex.ru/uslugi/api/1--/category/repetitoryi-i-obuchenie--2255??msp=no&p=0"
-	html = get_html(url)
-	print(get_page_data(html))
+	baseUrl = 'https://yandex.ru/uslugi/api/'
+	cityPart = '--/'
+	queryPart = 'category/repetitoryi-i-obuchenie'
+	categoryPart ='--2255??'
+	pagePart = 'msp=no&p='
+	cityArray = [1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 
+				22, 23, 24, 25, 28, 30, 33, 35, 36, 37, 38, 39, 41, 42, 43, 44, 45, 46, 47,
+				48, 49, 50, 51, 53, 54, 55, 56, 57, 58, 62, 63, 64, 65, 66, 67, 68, 74, 75, 
+				76, 77, 78, 79, 80]
+	nameCol()
+	for j in cityArray:
+		urlGenBase = baseUrl + str(j) + cityPart + queryPart + categoryPart + pagePart
+		for i in range(0, 100):
+			url = urlGenBase + str(i)
+			print(url)
+			html = getHtml(url)
+			print(getPageData(html))
 	
 
 if __name__ == '__main__':
