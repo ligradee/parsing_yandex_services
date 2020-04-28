269f9c08afd3d71dc5c72afa42b8e54a2a304a69
ligradee <57723993+ligradee@users.noreply.github.com>
2020-04-28 19:24:16 +0300

    get_page_data(html)
    I added function which loads html pages into json for further parsing.

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import requests
-
+import json
 
 def  get_html(url):
 	r = requests.get(url)
 	return r.text
 
+def get_page_data(html):
+	page = json.loads(html)
+
 def main():
 	url = "https://yandex.ru/uslugi/api/1--/category/repetitoryi-i-obuchenie--2255??msp=no&p=0"
+	html = get_html(url)
+	print(get_page_data(html))
 
 if __name__ == '__main__':
 	main()
